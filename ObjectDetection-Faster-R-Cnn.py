32b9e435bc38b5f59f259c8ae01272d49633a53d
Canberk Çinar <104972909+CanberkCnr@users.noreply.github.com>
2023-01-27 20:25:55 +0300

    Update ObjectDetection-Faster-R-Cnn.py

--- ObjectDetection-Faster-R-Cnn.py
+++ ObjectDetection-Faster-R-Cnn.py
@@ -14,15 +14,6 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 def get_predictions(pred,threshold=0.8,objects=None ):
-    """
-    This function will assign a string name to a predicted class and eliminate predictions whose likelihood  is under a threshold 
-    
-    pred: a list where each element contains a tuple that corresponds to information about  the different objects; Each element includes a tuple with the class yhat, probability of belonging to that class and the coordinates of the bounding box corresponding to the object 
-    image : frozen surface
-    predicted_classes: a list where each element contains a tuple that corresponds to information about  the different objects; Each element includes a tuple with the class name, probability of belonging to that class and the coordinates of the bounding box corresponding to the object 
-    thre
-    """
-
 
     predicted_classes= [(COCO_INSTANCE_CATEGORY_NAMES[i],p,[(int(box[0]), int(box[1])), (int(box[2]), int(box[3]))]) for i,p,box in zip(list(pred[0]['labels'].numpy()),pred[0]['scores'].detach().numpy(),list(pred[0]['boxes'].detach().numpy()))]
     predicted_classes=[  stuff  for stuff in predicted_classes  if stuff[1]>threshold ]
@@ -34,13 +25,6 @@ def get_predictions(pred,threshold=0.8,objects=None ):
 #Draws box around each object
 
 def draw_box(predicted_classes,image,rect_th= 10,text_size= 3,text_th=3):
-    """
-    draws box around each object 
-    
-    predicted_classes: a list where each element contains a tuple that corresponds to information about  the different objects; Each element includes a tuple with the class name, probability of belonging to that class and the coordinates of the bounding box corresponding to the object 
-    image : frozen surface 
-   
-    """
     img=(np.clip(cv2.cvtColor(np.clip(image.numpy().transpose((1, 2, 0)),0,1), cv2.COLOR_RGB2BGR),0,1)*255).astype(np.uint8).copy()
     for predicted_class in predicted_classes:
    
